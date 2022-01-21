a67b4e63214242438e8361dcaab00846ea06d75b
comp1 <comp1@COMP1-DNS>
2022-01-21 05:59:41 +0300

    admin.

--- Flats/Program.cs
+++ Flats/Program.cs
@@ -80,50 +80,72 @@ namespace Flats
                 var str = Console.ReadLine().Split();
                 int min = int.Parse(str[0]);
                 int max = int.Parse(str[1]);
-                showFlets($"select * from Flat where FullSquare between {max} and {min} order by id");
+                showFlets($"select * from Flat where FullSquare < {max} and FullSquare > {min} order by id");
             }
 
         }
 
         static void Main(string[] args)
-        {           
-           string name = autorise();
-            if (name == null)
-                return;
-            if(name == "admin")
+        {
+            while (true)
             {
-                Console.WriteLine($"Чем займёмся '{name}'");
-                showFlets("select * from Flat order by 'id'");
-                Console.Write("Чтобы добавить запись введите 'add' чтобы удалить 'del': ");
-                string? str = Console.ReadLine();
-                if(str == "add")
-                {
-                    Console.Write($"Введите значения полей через пробел" +
-                        $" (id rooms floore FullSquare Tenant): ");
-                    var adStr = Console.ReadLine().Split();
-                    int[] adInt = new int[adStr.Length - 1];
-                    for (int i = 0; i < adStr.Length - 1; i++)
-                        adInt[i] = int.Parse(adStr[i]);
-
-                    showFlets($"INSERT INTO Flat VALUES" +
-                        $" ({adInt[0]}, {adInt[1]}, {adInt[2]}, {adInt[3]}, {adStr[4]});");
+                string name = autorise();
+                if (name == null)
                     return;
+
+                if (name == "admin")
+                {
+                    Console.WriteLine($"Чем займёмся '{name}'");
+                    showFlets("select * from Flat order by 'id'");
+
+                    while (true)
+                    {
+                        Console.Write("Чтобы добавить запись введите 'add' чтобы удалить 'del': ");
+                        string? str = Console.ReadLine();
+                        if (str == "add")
+                        {
+                            Console.Write($"Введите значения полей через пробел" +
+                                $" (id rooms floore FullSquare Tenant): ");
+                            var adStr = Console.ReadLine().Split();
+                            int[] adInt = new int[adStr.Length - 1];
+                            for (int i = 0; i < adStr.Length - 1; i++)
+                                adInt[i] = int.Parse(adStr[i]);
+
+                            showFlets($"INSERT INTO Flat VALUES" +
+                                $" ({adInt[0]}, {adInt[1]}, {adInt[2]}, {adInt[3]}, {adStr[4]});");
+                            showFlets("select * from Flat order by 'id'");
+                            continue;
+                        }
+                        else if (str == "del")
+                        {
+                            Console.Write("Id строки которую хотите удалить: ");
+                            int del = int.Parse(Console.ReadLine());
+                            showFlets($"DELETE FROM Flat WHERE id = '{del}'");
+                            showFlets("select * from Flat order by 'id'");
+                            continue;
+                        }
+                        else break;
+                    }
+
                 }
+                if (name == "admin")
+                    continue;
+
+                    choice();
+
+                Console.Write("Какой вариант подходит? Введите 'id':");
+                var idFlat = Console.ReadLine();
+
+                using (var connection = new FlatDbConnection())
+                {
+                    var sql = $"update Flat set Tenant = '{name}' WHERE id = {idFlat}";
+                    using (var command = new SQLiteCommand(sql, connection.Sqlite))
+                        command.ExecuteNonQuery();
+                }
+
+                showFlets($"select * from Flat where Tenant = '{name}' or Tenant is null");
+                return;
             }
-
-            choice();
-
-            Console.Write("Какой вариант подходит? Введите 'id':");
-            var idFlat = Console.ReadLine();
-
-            using (var connection = new FlatDbConnection())
-            {
-                var sql = $"update Flat set Tenant = '{name}' WHERE id = {idFlat}";
-                using (var command = new SQLiteCommand(sql, connection.Sqlite))
-                    command.ExecuteNonQuery();
-            }
-
-            showFlets($"select * from Flat where Tenant = '{name}' or Tenant is null");
         }
     }
 }
